5d7b77aacb2ca2ffa1bddd7d399af3bc304dc68c
Vikas Ukani <vikasukani5@gmail.com>
2024-06-18 12:30:38 +0530

    update auth.

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,20 +1,24 @@
 const { createSlice } = require("@reduxjs/toolkit");
 
 const initialState = {
-    user: {},
-    token: null
-}
+  user: {},
+  showLoginPopup: false,
+  token: null,
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        setToken: (store, { payload }) => {
-            store.token = payload
-        },
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    setToken: (store, { payload }) => {
+      store.token = payload;
+    },
+    setShowLoginPopup: (store, { payload }) => {
+      store.showLoginPopup = payload;
+    },
+  },
+});
 
-export const { setToken } = authSlice.actions
+export const { setToken, setShowLoginPopup } = authSlice.actions;
 
-export default authSlice.reducer
+export default authSlice.reducer;
